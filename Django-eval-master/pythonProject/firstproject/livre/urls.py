b837dec6f40bc4ea98c7f3e1ab5f79e2362155d7
Matteo Cuney <matteo.cuney@uha.fr>
2022-05-09 15:04:52 +0200

    Initial commit

---
+++ Django-eval-master/pythonProject/firstproject/livre/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('formulaire', views.formulaire, name='formulaire'),
+    path('traitement/', views.traitement),
+    path("",views.index),
+    path("affiche/<int:id>/",views.affiche),
+    path("update/<int:id>/",views.update),
+    path("updatetraitement/<int:id>/",views.updatetraitement),
+    path("delete/<int:id>/",  views.delete),
+    path("accueil/", views.accueil),
+    path("formulairebibliotheque/", views.formulairebibliotheque),
+    path("affichebibliotheque/<int:id>/", views.affichebibliotheque),
+    path ("traitementbibliotheque/", views.traitementbibliotheque),
+    path("updatebibliotheque/<int:id>/", views.updatebibliotheque),
+    path("updatetraitementbibliotheque/<int:id>/", views.updatetraitementbibliotheque),
+    path ("deletebibliotheque/<int:id/", views.deletebibliotheque)
+
+]
+
---
+++ Django-eval-master/pythonProject/firstproject/livre/views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import LivreForm, BibliothequeForm
+from .import models
+
+
+def formulaire(request):
+        if request.method == "POST":
+                form = LivreForm(request)
+                return render(request, 'livre/formulaire.html', {'form': form})
+        else:
+                form = LivreForm()
+                return render(request, 'livre/formulaire.html', {'form': form})
+
+def traitement(request):
+    pForm = LivreForm(request.POST)
+    if pForm.is_valid():
+        livre = pForm.save()
+        return HttpResponseRedirect("/livre/")
+    else:
+        return render(request, 'livre/traitement.html', {'form': pForm})
+
+def index(request):
+    liste = list(models.Livre.objects.all())
+    return render(request,"livre/index.html",{"liste" : liste})
+
+def affiche(request, id):
+    livre = models.Livre.objects.get(pk=id)
+    return render(request,"livre/affiche.html",{"livre": livre})
+
+def update(request, id):
+    livre = models.Livre.objects.get(pk=id)
+    form = LivreForm(livre.dico())
+    return render(request,"livre/formulaire.html",{"form":form, "id": id})
+
+def updatetraitement(request, id):
+    pform = LivreForm(request.POST)
+    if pform.is_valid():
+        livre = pform.save(commit = False)
+        livre.id = id
+        livre.save()
+        return HttpResponseRedirect ("/livre/")
+    else:
+        return render(request, "livre/formulaire.html", {"form":pform, "id": id})
+
+def delete(request, id):
+    livre = models.Livre.objects.get(pk=id)
+    livre.delete()
+    return HttpResponseRedirect("/livre/")
+
+def accueil(request):
+    liste = list(models.Bibliotheque.objects.all())
+    return render(request,"Bibliotheque/accueil.html",{"liste" : liste})
+
+def formulairebibliotheque(request):
+    if request.method == "POST":
+        form = BibliothequeForm(request)
+        return render(request, 'Bibliotheque/formulairebibliotheque.html', {'form': form})
+    else:
+        form = BibliothequeForm()
+        return render(request, 'Bibliotheque/formulairebibliotheque.html', {'form': form})
+
+def affichebibliotheque(request, id):
+    bibli = models.Bibliotheque.objects.get(pk=id)
+    return render(request, "livre/affichebibliotheque.html", {"Bibliotheque": bibli})
+
+def traitementbibliotheque(request):
+    pForm = BibliothequeForm(request.POST)
+    if pForm.is_valid():
+        Bibliotheque = pForm.save()
+        return HttpResponseRedirect("/livre/affichebibliotheque")
+    else:
+        return render(request, 'livre/traitementbibliotheque.html', {'form': pForm})
+
+def updatebibliotheque(request, id):
+    Bibliotheque = models.Bibliotheque.objects.get(pk=id)
+    form = BibliothequeForm(Bibliotheque.dico())
+    return render(request,"livre/formulairebibliotheque.html",{"form":form, "id": id})
+
+def updatetraitementbibliotheque(request, id):
+    pform = BibliothequeForm(request.POST)
+    if pform.is_valid():
+        Bibliotheque = pform.save(commit = False)
+        Bibliotheque.id = id
+        Bibliotheque.save()
+        return HttpResponseRedirect ("/livre/")
+    else:
+        return render(request, "livre/formulairebibliotheque.html", {"form":pform, "id": id})
+
+def deletebibliotheque(request, id):
+    Bibliotheque = models.Bibliotheque.objects.get(pk=id)
+    Bibliotheque.delete()
+    return HttpResponseRedirect("/livre/")
+
+
+
